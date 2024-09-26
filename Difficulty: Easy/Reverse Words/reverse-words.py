f6b6d531112aa1386bf9a7de3724483fc021c72c
AMAR DEEP s <138798428+amargauge26@users.noreply.github.com>
2024-09-26 15:31:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse Words/reverse-words.py
@@ -0,0 +1,24 @@
+# User function Template for python3
+
+class Solution:
+    
+    #Function to reverse words in a given string.
+    def reverseWords(self,str):
+        # code here 
+        s= str.split('.')
+        s.reverse()
+        return ".".join(s)
+
+
+#{ 
+ # Driver Code Starts
+# Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        s = str(input())
+        obj = Solution()
+        print(obj.reverseWords(s))
+
+# } Driver Code Ends
