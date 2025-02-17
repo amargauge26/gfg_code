908e9d3953d4d343555aae79dbc60dbea47cfc67
AMAR DEEP s <138798428+amargauge26@users.noreply.github.com>
2025-02-17 23:22:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.py
+++ Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.py
@@ -3,44 +3,38 @@ class Solution:
 
 	# Function to find maximum
 	# product subarray
-	def maxProduct(self,arr, n):
+	def maxProduct(self,arr):
 		# code here
-		
-		pre = 1
-		suf =1
-		
 		maxi = float("-inf")
 		
-		for i in range(n):
-		    if pre ==0:
-		        pre =1
-		    
-		    if suf ==0:
+		pre =1
+		suf =1
+		n = len(arr)
+		for i in range(len(arr)):
+		  if pre==0:
+		        pre=1
+		       
+		  if suf==0:
 		        suf=1
 		    
-		    pre *=arr[i]
-		    
-		    suf*=arr[n-1-i]
-		    
-		    maxi = max(maxi,max(suf,pre))
-        
+		  pre *=arr[i]
+            
+          suf*=arr[n-1-i]
+            
+          maxi = max(maxi,max(suf,pre))
         
         return maxi
-
 
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
 
-
-
 if __name__ == '__main__':
     tc = int(input())
     while tc > 0:
-        n = int(input())
         arr = list(map(int, input().strip().split()))
         ob = Solution()
-        ans = ob.maxProduct(arr, n)
+        ans = ob.maxProduct(arr)
         print(ans)
         tc -= 1
 
