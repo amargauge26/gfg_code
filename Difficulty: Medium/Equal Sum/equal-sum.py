65ecd86260dae2c4a980f92415e765aa874fd918
Amar Deep Singh <138798428+amargauge26@users.noreply.github.com>
2025-03-06 16:38:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Equal Sum/equal-sum.py
+++ Difficulty: Medium/Equal Sum/equal-sum.py
@@ -3,23 +3,29 @@
 #Initial Template for Python 3
 
 # } Driver Code Ends
+
 #User function Template for python3
 class Solution:
 
 	def equilibrium(self,arr): 
     	# code here
-    	l_s=0
-    	t_s=sum(arr)
-    	
-    	for i in range(len(arr)):
-    	    r_s=t_s - l_s - arr[i]
-    	    
-    	    if r_s==l_s:
-    	        return "true"
-    	   
-    	    l_s+=arr[i]
-    	
-        return "false"
+    	n = len(arr)
+    	if n>=3:
+        	arrr=[0]*n
+        	arrl=[0]*n
+        	summ=0
+        	for i in range(n):
+        	    summ+=arr[i]
+        	    arrl[i]=summ
+        	summ=0
+            for i in range(n-1,-1,-1):
+        	    summ+=arr[i]
+        	    arrr[i]=summ
+            for i in range(1,n-1):
+                if arrl[i-1]==arrr[i+1]:
+                    return 'true'
+        return 'false'
+
 
 #{ 
  # Driver Code Starts.
@@ -32,6 +38,7 @@ if __name__ == "__main__":
         ob = Solution()
         res = ob.equilibrium(arr)
         print(res)
+        print("~")
         t -= 1
 
 
