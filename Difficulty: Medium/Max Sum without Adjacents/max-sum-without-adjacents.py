72a020f6d1131c3573c2f6fa8b5d042baf2bdbed
Amar Deep Singh <138798428+amargauge26@users.noreply.github.com>
2025-03-08 00:26:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Max Sum without Adjacents/max-sum-without-adjacents.py
+++ Difficulty: Medium/Max Sum without Adjacents/max-sum-without-adjacents.py
@@ -1,58 +1,41 @@
-#User function Template for python3
-class Solution:
-	
-	def findMaxSum(self,arr, n):
-		# code here
-		
-		
-		if n ==0:
-		    return 0
-		   
-        if n ==1:
-            return arr[0]
-            
-        
-	    dp = [-1]*n
-	    
-	    dp[0]=arr[0]
-	    dp[1]= max(arr[0],arr[1])
-	    
-	    
-	    
-	    for i in range(2,n):
-	        dp[i]=max(dp[i-2]+arr[i],dp[i-1])
-	       
-	    return dp[n-1]
-        
-    """def maxx(self,arr,ind,dp):
-        if ind <0:
-            return 0
-        
-        if dp[ind]!=-1:
-            return dp[ind]
-        
-        nottake = self.maxx(arr,ind-1,dp)
-        
-        take = arr[ind] + self.maxx(arr,ind-2,dp)
-        
-        dp[ind] = max(nottake,take)
-        return dp[ind]"""
-            
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
 
+# } Driver Code Ends
+
+#User function Template for python3
+class Solution:
+	
+	def findMaxSum(self,arr):
+		# code here
+		n = len(arr)
+		dp = [-1]*n
+        
+        dp[0]=arr[0]
+        dp[1]= max(arr[0],arr[1])
+        
+        
+        
+        for i in range(2,n):
+            dp[i]=max(dp[i-2]+arr[i],dp[i-1])
+           
+        return dp[n-1]
 
 
+#{ 
+ # Driver Code Starts.
+#Initial Template for Python 3
 
-if __name__ == '__main__':
-    tc = int(input())
-    while tc > 0:
-        n = int(input())
-        arr = list(map(int, input().strip().split()))
+if __name__ == "__main__":
+    t = int(input())
+    while t > 0:
+        arr = list(map(int, input().split()))
         ob = Solution()
-        ans = ob.findMaxSum(arr, n)
-        print(ans)
-        tc -= 1
+        res = ob.findMaxSum(arr)
+        print(res)
+        print("~")
+        t -= 1
+
 
 # } Driver Code Ends
