5ec3f27974b0a353a909dfeb7ae8e193624df72a
Amar Deep Singh <138798428+amargauge26@users.noreply.github.com>
2025-03-08 01:40:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Anagram/anagram.py
@@ -0,0 +1,23 @@
+from collections import Counter
+
+class Solution:
+    def areAnagrams(self, s1, s2):
+        return Counter(s1) == Counter(s2)
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        a = input().strip()
+        b = input().strip()
+        if (Solution().areAnagrams(a, b)):
+            print("true")
+        else:
+            print("false")
+        print("~")
+
+# } Driver Code Ends
