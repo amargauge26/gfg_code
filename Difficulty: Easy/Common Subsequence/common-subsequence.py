880eb7f31885885bbbf61873b167711564998a1f
Amar Deep Singh <138798428+amargauge26@users.noreply.github.com>
2025-03-08 15:06:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Common Subsequence/common-subsequence.py
+++ Difficulty: Easy/Common Subsequence/common-subsequence.py
@@ -2,12 +2,11 @@
 class Solution:
 	def commonSubseq(self, a, b):
 		# your code here
+         # your code here
         for i in a:
             for j in b:
                 if i==j:
                     return 1
-    
-    
         return 0
 
 #{ 
@@ -15,14 +14,16 @@ class Solution:
 #Initial Template for Python 3
 
 if __name__ == "__main__":
-	t = int (input ())
-	for tc in range (t):
-		a, b = input().split()
-		ob = Solution()
-		if ob.commonSubseq (a, b):
-			print (1)
-		else:
-			print (0)
+    t = int(input())
+    for tc in range(t):
+        a, b = input().split()
+        ob = Solution()
+        if ob.commonSubseq(a, b):
+            print(1)
+        else:
+            print(0)
+        print("~")
 
-	# Contributed By: Pranay Bansal
+    # Contributed By: Pranay Bansal
+
 # } Driver Code Ends
